3be66a8f065a0177fd9b8f7f3ebfb6c6210b337c
Henry Stuart Brown Berven <henber00@gmail.com>
2025-03-24 20:54:25 +0100

    Update PDF reader. can see pdf in terminal from PY_LLM

--- Modules/PDFReader.py
+++ Modules/PDFReader.py
@@ -5,27 +5,38 @@ import os
 def to_raw(string):
     return fr"{string}"
 
-
 def pdf_path(file_name):
     root_path = os.path.abspath(os.path.join("Archives", file_name))
     # Get the absolute path of the PDF file
     return root_path
 
-pdf_file = "BMW.pdf"
+def extract_text_from_pdf():
+    pdf_file = "BMW.pdf"
 
-pdf_file_path = pdf_path(pdf_file)
-output_file = "output.txt"
+    pdf_file_path = pdf_path(pdf_file)
+    output_file = "output.txt"
 
-try:
-    # Open the PDF file
-    doc = fitz.open(to_raw(pdf_file_path))
-    
-    with open(output_file, "w", encoding="utf-8") as out:
+    try:
+        # Open the PDF file
+        doc = fitz.open(to_raw(pdf_file_path))
+        out = open("output.txt", "w", encoding="utf-8")
+
         for page in doc:
             text = page.get_text()
-            out.write(text + "\n")  # Ensure line breaks
+            out.write(text + "\n")
+        out.close()
 
-    print(f"Text extracted successfully to {output_file}")
+        with open("output.txt", "r", encoding="utf-8") as file:
+            text = file.read()  
+        return text
 
-except Exception as e:
-    print(f"Error: {e}")
+
+#        with open(output_file, "w", encoding="utf-8") as out:
+#            for page in doc:
+#                text = page.get_text()
+#                out.write(text + "\n")  # Ensure line breaks
+#        print("this is a controll message no scam")
+#        return doc
+
+    except Exception as e:
+        print(f"Error: {e}")
--- Py_LLM.py
+++ Py_LLM.py
@@ -3,11 +3,14 @@ from ollama import chat
 from ollama import ChatResponse 
 from ollama import pull
 import json
+import Modules.PDFReader as PDFReader
 
 #Memory storage
 messages = [{"role": "user", "content": "You are a helpfull assistant named Paul. you will opperate like the assistent "
 "jarvis from the iron man movie's. ceep it brief and smooth. dont overdo it"}, 
 {"role": "assistant", "content": "Hello, how can I help you today?"},]
+
+PDFReader_Keyword = 'read pdf'
 
 #define model
 Model = 'llama3.2'
@@ -26,9 +29,15 @@ while True:
   
   totalResponse = ' '
 
-    # Add the response to the messages to maintain the history
+  if PDFReader_Keyword in user_input:
+    ex_pdf = PDFReader.extract_text_from_pdf()
+    print(ex_pdf)
+    messages.append({'role': 'user', 'content': ex_pdf})
+
+
+  # Add the response to the messages to maintain the history
   for chunk in stream:
-    
+  
     totalResponse += ' ' + chunk.message.content
         
     print(chunk.message.content, end='', flush=True)	
